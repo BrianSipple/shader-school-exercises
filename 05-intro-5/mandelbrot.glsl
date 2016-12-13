24579f7d0f725ca951258a4e49f23a823691bd67
BrianSipple <bsipple57@gmail.com>
2016-12-13 03:19:04 -0800

    05-intro-5/mandelbrot

--- 05-intro-5/mandelbrot.glsl
+++ 05-intro-5/mandelbrot.glsl
@@ -1,9 +1,21 @@
+bool mandelbrotConverges(highp vec2 z) {
+  return length(z) < 2.0;
+}
+
+vec2 _computeMandelbrot(highp vec2 z, highp vec2 c) {
+  return vec2( (z.x * z.x) - (z.y * z.y), 2.0 * z.x * z.y) + c;
+}
+
 bool mandelbrot(highp vec2 c) {
 
-  //Test if the point c is inside the mandelbrot set after 100 iterations
-  vec2 z = vec2(0.0);
+  vec2 z = vec2(0.0, 0.0);
 
-  return false;
+  // Test if the point c is inside the mandelbrot set after 100 iterations
+  for (int i = 0; i < 100; i++) {
+    z = _computeMandelbrot(z, c);
+  }
+
+  return mandelbrotConverges(z);
 }
 
 
