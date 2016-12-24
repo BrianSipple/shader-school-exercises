ba99326ba2364e369abf52d8907f24f1b1596974
BrianSipple <bsipple57@gmail.com>
2016-12-24 08:08:46 -0800

    06-intro-6/mpow

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,13 +1,27 @@
 mat2 matrixPower(highp mat2 m, int n) {
-  
-  //Raise the matrix m to nth power
+  // Raise the matrix m to nth power
 
   // For example:
   //
   //  matrixPower(m, 2) = m * m
   //
 
-  return mat2(1.0);  
+  const int MAX_POWER = 16;
+
+  if (n == 0) {
+    return mat2(1.0);
+  }
+
+  highp mat2 raisedMatrix = mat2(1.0) * m;
+
+  for (int i = 1; i < MAX_POWER; i++) {
+    if (i == n) {
+      return raisedMatrix;
+    }
+    raisedMatrix *= m;
+  }
+
+  return raisedMatrix;
 }
 
 //Do not change this line or the name of the above function
