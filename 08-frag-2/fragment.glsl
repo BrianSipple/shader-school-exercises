f4ca98a04eeace8431177ec96dee259ee702af5d
BrianSipple <bsipple57@gmail.com>
2016-12-24 18:56:46 -0800

    completed 08-frag-2/fragment

--- 08-frag-2/fragment.glsl
+++ 08-frag-2/fragment.glsl
@@ -1,8 +1,21 @@
 precision mediump float;
 
-void main() {
+bool inTile(vec2 pos, float tileSize) {
+  vec2 pTile = step(0.5, fract(0.5 * pos / tileSize));
 
-  //TODO: Replace this with a function that draws a checkerboard
-  
-  discard;
+  return pTile.x == pTile.y;
+}
+
+/**
+ * Implements a function that draws a checkerboard
+ */
+void main() {
+  float tileSize = 16.0;
+
+  if (inTile(gl_FragCoord.xy, tileSize)) {
+    gl_FragColor = vec4(1.0);
+
+  } else {
+    discard;
+  }
 }
