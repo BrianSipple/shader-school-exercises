4f4f846b1717be6fe6d23ff4fa13c07a050aa415
BrianSipple <bsipple57@gmail.com>
2017-01-06 00:58:13 -0800

    completed 09-frag-3

--- 09-frag-3/fragment.glsl
+++ 09-frag-3/fragment.glsl
@@ -3,10 +3,12 @@ precision highp float;
 uniform sampler2D texture;
 uniform vec2 screenSize;
 
+/**
+  * CHALLENGE: Swap red and blue color channels of image
+  */
 void main() {
   vec2 coord = gl_FragCoord.xy / screenSize;
+  vec4 originalColor = texture2D(texture, coord);
 
-  //TODO: Swap red and blue color channels of image
-
-  gl_FragColor = texture2D(texture, coord);
+  gl_FragColor = vec4(originalColor.b, originalColor.g, originalColor.r, originalColor.a);
 }
