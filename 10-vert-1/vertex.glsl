359d31333d6efe143e044d0ffbde551e60391429
BrianSipple <bsipple57@gmail.com>
2017-01-08 01:58:23 -0800

    solution for 10-vert-1/vertex.glsl

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -4,9 +4,24 @@ uniform float theta;
 
 attribute vec2 position;
 
+/**
+  * CHALLENGE: rotate position by theta radians about the origin
+  */
 void main() {
 
-  //TODO: rotate position by theta radians about the origin
+  float cosTheta = cos(theta);
+  float sinTheta = sin(theta);
 
-  gl_Position = vec4(position, 0, 1.0);
+  //  rotatation_matrx is | c    -s|
+  //                      | s    c|
+  // but glsl matrix constructor is column-major
+
+  mat2 rotationMatrix = mat2(
+    cosTheta, sinTheta,
+    -sinTheta, cosTheta
+  );
+
+  vec2 rotatedPosition = rotationMatrix * position;
+
+  gl_Position = vec4(rotatedPosition.xy, 0.0, 1.0);
 }
