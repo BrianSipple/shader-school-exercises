41f11e21542f25c61ea9aa11bb20e0573581227e
BrianSipple <bsipple57@gmail.com>
2017-01-22 19:45:48 -0800

    solution for `13-geom-2/translate`

--- 13-geom-2/translate.glsl
+++ 13-geom-2/translate.glsl
@@ -1,11 +1,18 @@
+/**
+ * CHALLENGE: Construct a matrix, m, which translates all
+ * points so that p is at the origin.
+ */
 highp mat4 translate(highp vec3 p) {
+  vec3 toOrigin = vec3(-p.x, -p.y, -p.z);
 
-  //TODO: Construct a matrix, m, which translates all points so that p is at the origin.
+  mat4 translationMatrix = mat4(
+    1,          0,          0,          0,
+    0,          1,          0,          0,
+    0,          0,          1,          0,
+    toOrigin.x, toOrigin.y, toOrigin.z, 1
+  );
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+  return translationMatrix;
 }
 
 //Do not remove this line
