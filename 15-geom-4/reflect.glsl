afc72759c198c111713ef11261dee80d1c371280
BrianSipple <bsipple57@gmail.com>
2017-02-03 06:47:53 -0800

    solution for `15-geom-4/reflect`

--- 15-geom-4/reflect.glsl
+++ 15-geom-4/reflect.glsl
@@ -3,15 +3,12 @@
  * about the plane passing through the origin with normal n
  */
 highp mat4 reflection(highp vec3 n) {
-
-  vec3 reflectedNormal = reflectPoint(vec3(0, 0, 0), n);
-
   vec3 axis = normalize(n);
 
   return mat4(
     1.0 - 2.0 * axis.x * axis.x,  -2.0 * axis.y * axis.x,        -2.0 * axis.z * axis.x,       0,
-    -2.0 * axis.x * axis.y,       1.0 - 2.0 * axis.y * axis.y,   -2.0 * axis.z * axis.y,       0,
-    -2.0 * axis.x * axis.z,       -2.0 * axis.y * axis.z,        1.0 -2.0 * axis.z * axis.z,   0,
+    -2.0 * axis.y * axis.x,       1.0 - 2.0 * axis.y * axis.y,   -2.0 * axis.z * axis.y,       0,
+    -2.0 * axis.z * axis.x,       -2.0 * axis.z * axis.y,        1.0 - 2.0 * axis.z * axis.z,  0,
     0,                            0,                             0,                            1
   );
 }
