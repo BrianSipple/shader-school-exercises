1bcb219a195b9c779d555050d3dc7a09506f834e
BrianSipple <bsipple57@gmail.com>
2017-02-20 07:43:42 -0800

    solution for 19-light-3

--- 19-light-3/fragment.glsl
+++ 19-light-3/fragment.glsl
@@ -5,6 +5,36 @@ uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, specular, lightDirection;
 uniform float shininess;
 
+varying vec3 vEyeDirection;
+varying vec3 vSurfaceNormal;
+
+
+float phongWeight(
+  vec3 surfaceNormal,
+  vec3 eyeDirection,
+  vec3 lightDirection,
+  float shininessFactor
+) {
+  vec3 reflectedLight = reflect(-lightDirection, surfaceNormal);
+  float eyeLightIntensity = max(0.0, dot(reflectedLight, eyeDirection));
+
+  return pow(eyeLightIntensity, shininessFactor);
+}
+
+float lambertWeight(vec3 surfaceNormalInWorldSpace, vec3 lightDirection) {
+  return max(0.0, dot(surfaceNormalInWorldSpace, lightDirection));
+}
+
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  float _phongWeight = phongWeight(vSurfaceNormal, vEyeDirection, lightDirection, shininess);
+  float _lambertWeight = lambertWeight(vSurfaceNormal, lightDirection);
+
+  vec3 color = (
+    ambient +
+    diffuse * _lambertWeight +
+    specular * _phongWeight
+  );
+
+  gl_FragColor = vec4(color, 1.0);
 }
--- 19-light-3/vertex.glsl
+++ 19-light-3/vertex.glsl
@@ -6,6 +6,15 @@ uniform mat4 inverseModel, inverseView, inverseProjection;
 uniform vec3 ambient, diffuse, specular, lightDirection;
 uniform float shininess;
 
+varying vec3 vEyeDirection;
+varying vec3 vSurfaceNormal;
+
 void main() {
-  gl_Position = vec4(position, 1);
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  vec4 worldNormal = vec4(normal, 0.0) * inverseModel * inverseView;
+
+  vEyeDirection = normalize(position - viewPosition.xyz);
+  vSurfaceNormal = worldNormal.xyz;
+
+  gl_Position = projection * viewPosition;
 }
