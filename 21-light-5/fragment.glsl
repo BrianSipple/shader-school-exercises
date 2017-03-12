eaa929eebf9a48fa79da4e22d75c4086b44fccf8
BrianSipple <brsipple42@gmail.com>
2017-03-12 03:17:57 -0700

    solution for 21-light-5

--- 21-light-5/fragment.glsl
+++ 21-light-5/fragment.glsl
@@ -2,18 +2,48 @@ precision mediump float;
 
 #pragma glslify: PointLight = require(./light.glsl)
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-uniform mat4 inverseModel;
-uniform mat4 inverseView;
-uniform mat4 inverseProjection;
-
 uniform vec3 ambient;
 
 uniform PointLight lights[4];
 
+varying vec3 vLightDirections[4];
+varying vec3 vSurfaceNormal;
+varying vec3 vSurfacePosition;
+
+float diffuseWeight(vec3 surfaceNormal, vec3 directionToLight) {
+  return max(0.0, dot(surfaceNormal, directionToLight));
+}
+
+float specularWeight(
+  vec3 surfaceNormal,
+  vec3 eyeDirection,
+  vec3 directionToLight,
+  float shiniessFactor
+) {
+  vec3 reflectedLight = reflect(directionToLight, surfaceNormal);
+  float eyeLightIntensity = max(0.0, dot(reflectedLight, eyeDirection));
+
+  return pow(eyeLightIntensity, shiniessFactor);
+}
+
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 eyeDirection = normalize(vSurfacePosition);
+  vec3 surfaceNormal = normalize(vSurfaceNormal);
+
+  // start with the ambient light
+  vec3 lightColor = ambient;
+
+  for (int i = 0; i < 4; i++) {
+    vec3 directionToLight = normalize(vLightDirections[i]);
+    float _diffuseWeight = diffuseWeight(surfaceNormal, directionToLight);
+    float _specularWeight = specularWeight(surfaceNormal, eyeDirection, directionToLight, lights[i].shininess);
+
+    vec3 diffuseColor = lights[i].diffuse * _diffuseWeight;
+    vec3 specularColor = lights[i].specular * _specularWeight;
+
+    lightColor += diffuseColor + specularColor;
+  }
+
+  gl_FragColor = vec4(lightColor, 1.0);
 }
--- 21-light-5/vertex.glsl
+++ 21-light-5/vertex.glsl
@@ -13,10 +13,24 @@ uniform mat4 inverseModel;
 uniform mat4 inverseView;
 uniform mat4 inverseProjection;
 
-uniform vec3 ambient;
-
 uniform PointLight lights[4];
 
+varying vec3 vSurfaceNormal;
+varying vec3 vSurfacePosition;
+varying vec3 vLightDirections[4];
+
 void main() {
-  gl_Position = vec4(position, 1);
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  vec4 viewNormal = vec4(normal, 0.0) * inverseModel * inverseView;
+
+  vSurfacePosition = viewPosition.xyz;
+  vSurfaceNormal = viewNormal.xyz;
+
+  for (int i = 0; i < 4; i++) {
+    vec4 viewLightPosition = view * vec4(lights[i].position, 1.0);
+
+    vLightDirections[i] = viewLightPosition.xyz - viewPosition.xyz;
+  }
+
+  gl_Position = projection * viewPosition;
 }
