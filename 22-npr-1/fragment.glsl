25cb20aa87e6bae882fcb68ea4d55d610cfd7773
BrianSipple <brsipple42@gmail.com>
2017-03-13 00:20:57 -0700

    solution for 22-npr-1

--- 22-npr-1/fragment.glsl
+++ 22-npr-1/fragment.glsl
@@ -12,6 +12,22 @@ uniform vec3 diffuse;
 uniform vec3 lightDirection;
 uniform float numBands;
 
+varying vec3 vSurfaceNormal;
+
+float diffuseWeight(vec3 surfaceNormal, vec3 directionToLight) {
+  return max(0.0, dot(surfaceNormal, directionToLight));
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec3 surfaceNormal = normalize(vSurfaceNormal);
+  vec3 nLightDirection = normalize(lightDirection);
+
+  float _diffuseWeight = diffuseWeight(surfaceNormal, nLightDirection);
+
+  // Transform the _diffuseWeight according to our cel shading logic
+  _diffuseWeight = ceil(_diffuseWeight * numBands) / numBands;
+
+  vec3 diffuseColor = diffuse * _diffuseWeight;
+
+  gl_FragColor = vec4(diffuseColor, 1.0);
 }
--- 22-npr-1/vertex.glsl
+++ 22-npr-1/vertex.glsl
@@ -15,6 +15,18 @@ uniform vec3 diffuse;
 uniform vec3 lightDirection;
 uniform float numBands;
 
+varying vec3 vSurfaceNormal;
+
+
+/**
+ * CHALLENGE: Modify the Lambert diffuse lighting model
+ * from `light-2` to support cel shading
+ */
 void main() {
-  gl_Position = vec4(position,1);
+  vec4 viewPosition = view * model * vec4(position, 1.0);
+  vec4 worldNormal = vec4(normal, 0.0) * inverseModel;
+
+  vSurfaceNormal = worldNormal.xyz;
+
+  gl_Position = projection * viewPosition;
 }
