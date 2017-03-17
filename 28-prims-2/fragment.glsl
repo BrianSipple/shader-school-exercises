9714a26f60e59bcd16b85fe8f2541f9b998afd7b
BrianSipple <brsipple42@gmail.com>
2017-03-16 17:47:11 -0700

    solution for `28-prims-2`

--- 28-prims-2/fragment.glsl
+++ 28-prims-2/fragment.glsl
@@ -2,6 +2,16 @@ precision highp float;
 
 uniform vec3 frontColor, backColor;
 
+/**
+ * Write a fragment shader which colors fragments depending on their relative
+ * orientation to the view direction.
+ *
+ * If they are facing away from the camera, color them with `backColor`.
+ * If they are facing towards the camera color them with `frontColor`.
+ */
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+  vec3 color = gl_FrontFacing ? frontColor : backColor;
+
+  gl_FragColor = vec4(color, 1.0);
 }
