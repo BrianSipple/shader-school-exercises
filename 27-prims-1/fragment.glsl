d0994155526b0865309c8dbe82d1208061069f98
BrianSipple <brsipple42@gmail.com>
2017-03-30 18:52:42 -0700

    solution for `27-prims-1`

--- 27-prims-1/fragment.glsl
+++ 27-prims-1/fragment.glsl
@@ -1,5 +1,15 @@
 precision highp float;
 
+varying float vSize;
+varying vec3 vColor;
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  vec2 xy = gl_PointCoord.st;
+  vec2 pointCenter = vec2(0.5, 0.5);
+
+  if (distance(xy, pointCenter) > 0.5) {
+    discard;
+  }
+
+  gl_FragColor = vec4(vColor, 1.0);
 }
--- 27-prims-1/vertex.glsl
+++ 27-prims-1/vertex.glsl
@@ -1,10 +1,26 @@
 precision highp float;
 
 attribute vec3 position, color;
+
+/**
+ * The desired size of the circle radius
+ */
 attribute float size;
 
 uniform mat4 model, view, projection;
 
+varying float vSize;
+varying vec3 vColor;
+
+
+/**
+ * CHALLENGE: Create a vertex/fragment shader pair to render a
+ * collection of colored point sprites.
+ */
 void main() {
+  vSize = size;
+  vColor = color;
+
+  gl_PointSize = size;
   gl_Position = projection * view * model * vec4(position, 1.0);
 }
